ce59012e54979a554dee30c1c9407dcf802e9573
Sri Sai Omkar Akilla <srisaiomkar@gmail.com>
2022-12-13 23:25:05 -0500

    create user

--- webparent/backend/src/main/java/com/magicmart/admin/user/UserController.java
+++ webparent/backend/src/main/java/com/magicmart/admin/user/UserController.java
@@ -5,6 +5,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.magicmart.common.entity.User;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+
 @Controller
 public class UserController {
     @Autowired
@@ -15,4 +20,21 @@ public class UserController {
         model.addAttribute("listUsers", userService.listAllUsers());
         return "users";
     }
+
+    @GetMapping("users/new")
+    public String createUserForm(Model model){
+        User user = new User();
+        user.setEnabled(true);
+        model.addAttribute("user", user);
+        model.addAttribute("listRoles", userService.listAllRoles());
+        return "user_form";
+    }
+
+    @PostMapping("users/save")
+    public String saveUser(User user, RedirectAttributes redirectAttributes) {
+        userService.saveUser(user);
+        redirectAttributes.addFlashAttribute("message", "The user has been saved successfully");
+        return "redirect:/users";
+    }
+    
 }
--- webparent/backend/src/main/java/com/magicmart/admin/user/UserService.java
+++ webparent/backend/src/main/java/com/magicmart/admin/user/UserService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.magicmart.common.entity.Role;
 import com.magicmart.common.entity.User;
 
 @Service
@@ -13,8 +14,18 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private RoleRepository roleRepository;
+
     public List<User> listAllUsers(){
         return (List<User>) userRepository.findAll();
+    }
+
+    public List<Role> listAllRoles(){
+        return (List<Role>) roleRepository. findAll();
+    }
+    public void saveUser(User user){
+        userRepository.save(user);
     }
     
 }
